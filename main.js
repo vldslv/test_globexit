051716840ba0d3fa434626c593d9c0272e3987b4
vldslv <v.aronovsky@yandex.ru>
2020-11-26 06:39:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let colorChanger = function() {
-    let color = document.querySelector("#color").value;
-    let number = document.querySelector("#number").value;
-    let elements = document.querySelectorAll("td");
-    let active_element = document.querySelector(`.cell-${number}`);
+    let color = document.querySelector("#color").value,
+        number = document.querySelector("#number").value,
+        elements = document.querySelectorAll("td"),
+        active_element = document.querySelector(`.cell-${number}`);
     
     for (let element of elements) {
         element.style.backgroundColor = color;
